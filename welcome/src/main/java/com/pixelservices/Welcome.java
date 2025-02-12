91d566f57471f3b43372ccd1c2be7d89e3082d69
SIEA <69807609+DrVosss@users.noreply.github.com>
2025-02-12 17:42:58 +0100

    added welcome module

---
+++ welcome/src/main/java/com/pixelservices/Welcome.java
@@ -0,0 +1,15 @@
+package com.pixelservices;
+
+import com.pixelservices.api.modules.MbModule;
+
+public class Welcome extends MbModule {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
